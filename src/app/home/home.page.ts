656954c0a5326afcecc286313c9ea4e2c98f9555
Mikee <mikeed@sourcepad.com>
2018-11-07 00:29:39 +0800

    Init pug + gitignore

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
+  templateUrl: 'home.page.pug',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
